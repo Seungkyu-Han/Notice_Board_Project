62b0bf96ce2bd42a10eff33f45836a9ce76f25ef
Seungkyu-Han <trust1204@gmail.com>
2024-10-25 21:15:06 +0900

    feat: add Category API Service

--- post/core/src/main/kotlin/seungkyu/board/service/CategoryService.kt
+++ post/core/src/main/kotlin/seungkyu/board/service/CategoryService.kt
@@ -1,5 +1,12 @@
 package seungkyu.board.service
 
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+
 
 interface CategoryService {
+
+    suspend fun create(serverRequest: ServerRequest): ServerResponse
+    suspend fun update(serverRequest: ServerRequest): ServerResponse
+    suspend fun delete(serverRequest: ServerRequest): ServerResponse
 }
--- post/core/src/main/kotlin/seungkyu/board/service/impl/CategoryServiceImpl.kt
+++ post/core/src/main/kotlin/seungkyu/board/service/impl/CategoryServiceImpl.kt
@@ -1,13 +1,52 @@
 package seungkyu.board.service.impl
 
-import org.slf4j.LoggerFactory
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.buildAndAwait
+import seungkyu.board.dto.req.CreateReq
+import seungkyu.board.dto.req.UpdateReq
+import seungkyu.board.entity.CategoryDocument
+import seungkyu.board.repository.CategoryMongoRepository
 import seungkyu.board.service.CategoryService
 
 @Service
-class CategoryServiceImpl: CategoryService {
+class CategoryServiceImpl(
+    private val categoryMongoRepository: CategoryMongoRepository
+): CategoryService {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(CategoryServiceImpl::class.java)
+    override suspend fun create(serverRequest: ServerRequest): ServerResponse {
+
+        val createReq = serverRequest.bodyToMono(CreateReq::class.java).awaitSingle()
+
+        val category = CategoryDocument(name = createReq.name)
+
+        categoryMongoRepository.save(category).awaitSingle()
+
+        return ServerResponse.status(201).buildAndAwait()
+    }
+
+    override suspend fun update(serverRequest: ServerRequest): ServerResponse {
+        val updateReq = serverRequest.bodyToMono(UpdateReq::class.java).awaitSingle()
+
+        val category = categoryMongoRepository.findByName(updateReq.beforeName).awaitSingle()
+
+        category.name = updateReq.afterName
+
+        categoryMongoRepository.save(category).awaitSingle()
+
+        return ServerResponse.status(201).buildAndAwait()
+    }
+
+    override suspend fun delete(serverRequest: ServerRequest): ServerResponse {
+        val categoryName = serverRequest.queryParam("name")
+
+        if(categoryName.isEmpty)
+            return ServerResponse.badRequest().buildAndAwait()
+
+        categoryMongoRepository.deleteByName(categoryName.get()).awaitSingleOrNull()
+        return ServerResponse.ok().buildAndAwait()
     }
 }
